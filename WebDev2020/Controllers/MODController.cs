e4aff2a115a636d09d3b55d0bbe80150925d4047
Mr-Jabberwock <djohnsen143@gmail.com>
2020-12-02 11:04:03 +0100

    Nu med iteration af dummy data

---
+++ WebDev2020/Controllers/MODController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDev2020.Models;
+
+namespace WebDev2020.Controllers
+{
+    public class MODController : Controller
+    {
+        // GET: MOD
+        public ActionResult Blog()
+        {
+            List<Blog> blog = new List<Blog>();
+            Blog blog1 = new Blog { title = "blog", post = "Posty", firstName = "John", lastName = "Doe", urlPath = "dr.dk", userID = 1 };
+            Blog blog2 = new Blog { title = "blog2", post = "Posty2", firstName = "John2", lastName = "Doe2", urlPath = "tv2.dk", userID = 2 };
+            blog.Add(blog1);
+            blog.Add(blog2);
+
+            ViewData["Data"] = blog;
+
+            return View();
+
+        }
+    }
+}
---
+++ WebDev2020/Models/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDev2020.Models
+{
+    public class Blog
+    {
+        public String title { get; set; }
+        public int userID { get; set; }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String post { get; set; }
+        public String urlPath { get; set; }
+}
+}
