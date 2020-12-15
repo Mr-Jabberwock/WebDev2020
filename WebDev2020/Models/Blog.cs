ead557c7a314f48424110c6fe644fc6d71daa9cc
pratheesravindrakumar <prat0025@edu.ucl.dk>
2020-12-15 14:10:29 +0100

    * Blog.cs:
    * WebDev2020-Debug.json: Get Set

---
+++ WebDev2020/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WebDev2020.Models
+{
+    public class Blog
+    {
+        
+        public String title { get; set; }
+        public String userId { get; set; }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String post { get; set; }
+        public String urlPath { get; set; }
+    
+    }
+}
